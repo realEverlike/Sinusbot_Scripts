d2e4a02bb0b5605eb9b1ede75f2d64ea598f72d5
Everlike <admin@everlike.de>
2018-04-02 23:52:06 +0200

    pizzaTimer.js
    Description
    This simple script will remind you´r clients of their food in the oven!
    Just write "!pizza 5" via private message to the bot and the bot will write you a message 5 minutes later, to remind you of you food! The client can vary the time by just changing the number behind !pizza.
    
    
    must-have-permissions
    "i_client_private_textmessage_power"
    "i_client_poke_power"
    
    Note:
    Here is what you can change via Sinusbot Webinterface
    Command or Message which triggers the bot to set the timer.
    => default: "!pizza"
    The bot will send the following message to remind the client.
    => default: "Your pizza is ready! Stop gaming now and get the pizza out of the oven!"
    Message or Poke the client when the pizza is ready?
    => default: "poke"
    Confirmation message that the timer has been set, and the bot will write a message to the client.
    => default: "Alright, I´ll write you a message when your pizza is ready!"
    Confirmation message that the timer has been set, and the bot will poke the client.
    => default: "Alright, I´ll poke you when your pizza is ready!"
    The bot will poke the client in the following amount of time by default in minutes.
    => default: "13"

---
+++ pizzaTimer.js
@@ -0,0 +1,93 @@
+registerPlugin({
+    name: 'PizzaTimer',
+    version: '0.5',
+    description: 'Do never forget your pizza in the oven again!',
+    author: 'Everlike <everlikezz@gmail.com>',
+    vars: [
+       	{
+		name: 'triggermsg',
+		title: 'Command or Message which triggers the bot to set the timer.',
+		type: 'string', 
+		placeholder: '!pizza'
+	},
+	   {
+            name: 'msg',
+            title: 'The bot will send the following message to remind the client.',
+            type: 'string',
+            placeholder: 'Your pizza is ready! Stop gaming now and get the pizza out of the oven!'
+        },
+    {
+        name: 'msgorpoke',
+        title: 'Message or Poke the client when the pizza is ready?',
+        type: 'select',
+        placeholder: 'message',
+        options: [
+        'message',
+        'poke'
+        ]
+    },
+    {
+        name: 'confirmmsg',
+        title: 'Confirmation message that the timer has been set, and the bot will write a message to the client.',
+        type: 'string',
+        placeholder: 'Alright, I´ll write you a message when your pizza is ready!'
+    },
+    {
+        name: 'confirmpoke',
+        title: 'Confirmation message that the timer has been set, and the bot will poke the client.',
+        type: 'string',
+        placeholder: 'Alright, I´ll poke you when your pizza is ready!'
+    },
+	{
+		name: 'defaulttime',
+		title: 'The bot will poke the client in the following amount of time by default in minutes.',
+		type: 'number',
+		placeholder: 13
+	},
+]
+},  function (sinusbot, config) {
+   
+    var engine = require ('engine');
+    var backend = require ('backend');
+    var event = require ('event');
+ 
+    var msg = config.msg || 'Your pizza is ready! Stop gaming now and get the pizza out of the oven!';
+    var msgorpoke = config.msgorpoke || 1;
+    var confirmmsg = config.confirmmsg || 'Alright, I´ll write you a message when your pizza is ready!';
+    var confirmpoke = config.confirmpoke || 'Alright, I´ll poke you when your pizza is ready!';
+	var defaulttime = config.defaulttime || 13;
+	var t1 = '^';
+	var t2 = ' *(\\d*)$';
+	var triggermsg = t1 + config.triggermsg + t2 || '(/^!pizza *(\d*)$/)';
+	
+    event.on('chat', function (ev) {
+		var match = ev.text.match(new RegExp(triggermsg))
+		
+        if (match && msgorpoke == 0) {
+            {
+                ev.client.chat(confirmmsg)
+            }
+            setTimeout(function(){
+                ev.client.chat(msg)
+            }, parseInt(match[1] || defaulttime) * 60000)
+        }
+		else if (match && msgorpoke == 1) {
+            {
+                ev.client.chat(confirmpoke)
+            }
+            setTimeout(function(){
+                ev.client.poke(msg)
+            }, parseInt(match[1] || defaulttime) * 60000)
+        }
+		// This is just for debug reasons. 
+		/*
+		else if (ev.text.match('tellme')) {
+			ev.client.chat(triggermsg)
+		}
+		else if (ev.text.match('tellme')) {
+			ev.client.chat(triggermsg)
+		}
+		*/
+		
+    });
+});
